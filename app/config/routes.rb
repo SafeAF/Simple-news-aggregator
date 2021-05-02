fc390db74ff53e9fbeaebaee2438204083b3d7a2
Samuel Kerr <transiencymail@gmail.com>
2021-05-02 10:47:03 -0600

    showing replies index, destroy works

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   end
 
   resources :comments
+  resources :replies
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
