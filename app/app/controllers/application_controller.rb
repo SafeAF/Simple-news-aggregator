fe71c5e5c948fc7c06f4cd3ff173c44ea8919bab
Samuel Kerr <transiencymail@gmail.com>
2021-05-05 06:28:04 -0600

    added bio update to devise

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -25,9 +25,9 @@ class ApplicationController < ActionController::Base
      protected
 
           def configure_permitted_parameters
-               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password)}
+               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :bio)}
 
-               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password)}
+               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :bio)}
           end
 
 
