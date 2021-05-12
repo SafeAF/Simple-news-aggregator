deb2047afa3d371eb02e96a4d6eeae2df90a1207
Samuel Kerr <transiencymail@gmail.com>
2021-05-12 08:41:08 -0600

    more rendering fixes

--- app/app/controllers/home_controller.rb
+++ app/app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
 	def index
-		 @newest_posts = Post.order(created_at: :desc).limit(10)
+		 @newest_posts = Post.order(created_at: :desc, cached_votes_up: :desc).limit(10)
 		 #@top_posts = Post.order(cached_votes_up: :desc).limit(30)
 	end
 end
